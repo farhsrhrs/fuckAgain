6aa255f6874e21eb592ba76f060b1895d8b7c5b4
farhsrhrs <aleksei_protopopov_06@mail.ru>
2025-12-25 06:59:27 +0300

    all

---
+++ fuckAgain/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using fuckAgain.Properties;
+using Npgsql;
+
+namespace fuckAgain
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+
+            InitializeComponent();
+            LoadTavar();
+
+        }
+        public void LoadTavar()
+        {
+            using (NpgsqlConnection conaction = new NpgsqlConnection(Resources.ConactionDB))
+            {
+                conaction.Open();
+
+                string query = @"SELECT id_artical, name_tovar.name_tovar_pk, edinic_izm,  price, postavshic.name_postavshic_pk , proizvoditel.name_proizvoditel_pk, category.category_pk, discount, quantity, description, photo
+	FROM public.tovar
+	JOIN name_tovar on name_tovar.id = public.tovar.name_tovar_fk
+	JOIN postavshic on postavshic.id = tovar.postavshic_fk
+	JOIN proizvoditel on proizvoditel.id = tovar.proizvoditel_fk
+	JOIN category on category.id = tovar.category_fk	
+                ";
+                using (NpgsqlCommand command = new NpgsqlCommand(query, conaction))
+                {
+                    using (NpgsqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            //int id = Convert.ToString(reader["id_artical"]);
+                            var tovar = new UserControl1();
+                            PopulateTovarFromReader(reader, tovar);
+                            tovar.Labels();
+                            flowLayoutPanel1.Controls.Add(tovar);
+                        }
+                        flowLayoutPanel1.ResumeLayout(true);
+                    }
+
+
+
+
+
+                }
+            }
+
+        }
+        private void PopulateTovarFromReader(NpgsqlDataReader reader, UserControl1 tovar)
+        {
+            tovar.name_tovar = reader.IsDBNull(1) ? "" : reader.GetString(1);
+
+        }
+    }
+}
---
+++ fuckAgain/UserControl1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace fuckAgain
+{
+    public partial class UserControl1 : UserControl
+    {
+        public string name_tovar { get; set; }
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+        public void Labels()
+        {
+            label1.Text = "Наименование: " + name_tovar;
+        }
+    }
+}
